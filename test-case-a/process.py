8e2b484625c784b30b847d6c6f95d4a35efeb138
Jesús Castro <x51v4n@gmail.com>
2020-03-04 12:30:15 -0600

    Test case a:
    On this case, we ensure that the output produced by the cpp program
    works under the python execution.
    
    [ticket: X]
    
    Signed-off-by: Jesús Castro <x51v4n@gmail.com>

---
+++ test-case-a/process.py
@@ -0,0 +1,10 @@
+from subprocess import Popen, PIPE
+
+p = Popen(['./a.out'], shell=True, stdout=PIPE, stdin=PIPE)
+for ii in range(10):
+    value = str(ii) + '\n'
+    value = bytes(value, 'UTF-8')  # Needed in Python 3.
+    p.stdin.write(value)
+    p.stdin.flush()
+    result = p.stdout.readline().strip()
+    print(result)
